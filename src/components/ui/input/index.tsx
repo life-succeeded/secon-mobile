3582b7dd605adb1936d86817bbe0cd72f709edd1
quassbottle <quassbottleyt@gmail.com>
2025-04-11 00:18:46 +0300

    fix input

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -13,7 +13,7 @@ export const Input = (props: IInputProps) => {
             <input
                 type="text"
                 className={twMerge(
-                    'border-white-3 placeholder-grey-3 h-[44px] rounded-[6px] border px-4 py-3',
+                    'border-white-3 placeholder-grey-3 focus:border-black-2 h-[44px] rounded-[6px] border px-4 py-3 focus:border-[2px]',
                     props.className,
                 )}
                 value={props.value}

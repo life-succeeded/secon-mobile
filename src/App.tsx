a86ae7a1a6296a5e55eac482b758e4d688cee998
quassbottle <quassbottleyt@gmail.com>
2025-04-12 00:26:18 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { AppRoutes } from './lib/routes';
-import { Header } from './components/ui/header';
-import { Navbar } from './components/ui/navbar';
-import { NavigationTracker } from './lib/NavigationTracker';
+import { AppRoutes } from './lib/routes'
+import { Header } from './components/ui/header'
+import { Navbar } from './components/ui/navbar'
+import { NavigationTracker } from './lib/NavigationTracker'
 
 function App() {
-  return (
-      <div className="flex h-screen flex-col">
-        <Header />
-    
-        <main className="flex-1 overflow-auto p-4">
-          <AppRoutes />
-          <NavigationTracker />
-        </main>
-        
-        <Navbar />
-      </div>
-  );
+    return (
+        <>
+            <AppRoutes />
+            <NavigationTracker />
+        </>
+    )
 }
 
 export default App

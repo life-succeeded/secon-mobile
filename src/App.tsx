10b55af879ed9ca5f97214156ef8b358db0d0151
quassbottle <quassbottleyt@gmail.com>
2025-04-12 00:25:38 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { BrowserRouter } from 'react-router'
 import { AppRoutes } from './lib/routes'
-import { Header } from './components/ui/header'
-import { Navbar } from './components/ui/navbar'
 
 function App() {
     return <AppRoutes />

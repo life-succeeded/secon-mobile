9c7064865228edc156e762ad5dfb99af6303a3c6
rh0danthe <rita.pichushkina.04@mail.ru>
2025-04-12 21:14:11 +0300

    act create

--- src/pages/ActCreate.tsx
+++ src/pages/ActCreate.tsx
@@ -2,18 +2,22 @@ import { useSelector } from 'react-redux';
 import ContactInfo from './act-create/ContactInfo';
 import { RootState } from '../store/store';
 import ActType from './act-create/ActType';
+import SwitchingDevice from './act-create/SwitchingDevice';
 
 function ActCreate() {
-    const { currentStep } = useSelector((state: RootState) => state.navigation.formSteps);
-    
+    const { currentStep } = useSelector((state: RootState) => state.navigation.formSteps)
+
     return (
-        <div className="form-container">
-            {currentStep === 1 && <ContactInfo />}
-            {currentStep === 2 && <ActType />}
-            {currentStep === 3 && <ActType />}
-            {currentStep === 4 && <ActType />}
+        <div className="flex flex-col h-[80vh]"> 
+            <div className="flex-grow overflow-auto">
+                {currentStep === 1 && <ContactInfo />}
+                {currentStep === 2 && <ActType />}
+                {currentStep === 3 && <SwitchingDevice />}
+                {currentStep === 4 && <ActType />}
+            </div>
         </div>
     )
 }
+
 
 export default ActCreate;
--- src/pages/act-create/ContactInfo.tsx
+++ src/pages/act-create/ContactInfo.tsx
@@ -1,88 +1,74 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
-import { updateFormState, nextFormStep } from '../../store/navigationSlice';
-import { ProgressBar } from '../../components/ui/progressbar';
-import { Input } from '../../components/ui/input';
-import { Button } from '../../components/ui/button';
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../store/store'
+import { updateFormState, nextFormStep } from '../../store/navigationSlice'
+import { Input } from '../../components/ui/input'
+import { Button } from '../../components/ui/button'
 
 function ContactInfo() {
-    const dispatch = useDispatch();
-    const { formState, currentStep } = useSelector((state: RootState) => state.navigation.formSteps);
+    const dispatch = useDispatch()
+    const { formState, currentStep } = useSelector((state: RootState) => state.navigation.formSteps)
 
     const handleNext = () => {
         if (!formState.phoneNumber?.trim()) {
-            alert('Пожалуйста, введите контактный номер');
-            return;
+            alert('Пожалуйста, введите контактный номер')
+            return
         }
-        console.log({currentStep})
+        console.log({ currentStep })
 
-        dispatch(nextFormStep());
-    };
+        dispatch(nextFormStep())
+    }
 
     return (
-        <div className="flex h-screen flex-col">
-            <div className="mx-5 mt-25 flex-grow overflow-auto">
-                <div className="flex w-full flex-col justify-center gap-5">
-                    <ProgressBar 
-                        value={currentStep} 
-                        className="self-center" 
-                    />
-                    
-                    <div className="flex w-full flex-col justify-center gap-3">
-                        <div className = "flex flex-col gap-2">
-                        <label className="text-14-20-regular">
-                            Контактный номер
-                        </label>
-                        <Input 
+        <div className="flex flex-col h-full relative px-5 pt-25">
+                <div className="flex w-full flex-col gap-5">
+                    <div className="flex w-full flex-col gap-3">
+                        <Input
+                            label={'Контактный номер'}
                             value={formState.phoneNumber || ''}
-                            placeholder="Введите контактный номер" 
+                            placeholder="Введите контактный номер"
                             onChange={(e) =>
-                                dispatch(updateFormState({ 
-                                    phoneNumber: e.target.value 
-                                }))
+                                dispatch(
+                                    updateFormState({
+                                        phoneNumber: e.target.value,
+                                    }),
+                                )
                             }
                         />
-                        </div >
-                        <div className = "flex flex-col gap-2">
-                        <label className="text-14-20-regular">
-                            Потребитель
-                        </label>
-                        <Input 
+                        <Input
+                            label={'Потребитель'}
                             value={formState.consumer || ''}
-                            placeholder="Введите ФИО потребителя" 
+                            placeholder="Введите ФИО потребителя"
                             onChange={(e) =>
-                                dispatch(updateFormState({ 
-                                    phoneNumber: e.target.value 
-                                }))
+                                dispatch(
+                                    updateFormState({
+                                        consumer: e.target.value,
+                                    }),
+                                )
                             }
                         />
-                        </div>
-                        <div className = "flex flex-col gap-2">
-                        <label className="text-14-20-regular">
-                            Объект
-                        </label>
-                        <Input 
-                            value={formState.address || ''}
-                            placeholder="Введите адрес объекта" 
-                            onChange={(e) =>
-                                dispatch(updateFormState({ 
-                                    phoneNumber: e.target.value 
-                                }))
-                            }
-                        />
+
+                        <div className="flex flex-col gap-2">
+                            <label className="text-14-20-regular">Объект</label>
+                            <Input
+                                value={formState.address || ''}
+                                placeholder="Введите адрес объекта"
+                                onChange={(e) =>
+                                    dispatch(
+                                        updateFormState({
+                                            address: e.target.value,
+                                        }),
+                                    )
+                                }
+                            />
                         </div>
                     </div>
-                    
-                    <Button 
-                        onClick={handleNext}
-                        className='mt-28'
-                    >
-                        Продолжить
-                    </Button>
                 </div>
+                <div className="absolute top-130 left-5 right-5">
+                <Button className="w-full" onClick={handleNext}>
+                    Продолжить
+                </Button>
             </div>
         </div>
     )
 }
-
-export default ContactInfo;
+export default ContactInfo

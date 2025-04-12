306c2879eb660b74e16697a2ff31757def396da8
quassbottle <quassbottleyt@gmail.com>
2025-04-12 19:56:01 +0300

    fix

--- src/api/hooks/useGetBrigadeById.ts
+++ src/api/hooks/useGetBrigadeById.ts
@@ -1,10 +1,10 @@
 import useAxios from '../../lib/hooks/useAxios'
 import { IBrigade, ITask } from '../api.types'
 
-const useGetBrigadeById = (params: { id: string }) => {
+const useGetBrigadeById = (params: { id: string }, autoExecute = true) => {
     const { id } = params
 
-    return useAxios<IBrigade>(`/brigades/${id}`, 'get', true)
+    return useAxios<IBrigade>(`/brigades/${id}`, 'get', autoExecute)
 }
 
 export default useGetBrigadeById

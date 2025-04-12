62c9319cd26036996640b3cbe7dd59a3b8b86e96
quassbottle <quassbottleyt@gmail.com>
2025-04-13 01:41:17 +0300

    add radio and checkbox

--- src/components/ui/checkbox/index.tsx
+++ src/components/ui/checkbox/index.tsx
@@ -1,40 +1,66 @@
 import { useState } from 'react'
+import {
+    Control,
+    Controller,
+    FieldValues,
+    UseControllerProps,
+    useFormContext,
+} from 'react-hook-form'
 import { twMerge } from 'tailwind-merge'
 
 type TCheckboxChangeEvent = React.ChangeEvent<HTMLInputElement>
 
-interface ICheckboxProps {
+interface ICheckboxProps<TFormValues extends FieldValues = FieldValues, TValue = string> {
     label?: string
     checked?: boolean
     className?: string
+    value: TValue
     onChange?: (e: TCheckboxChangeEvent) => void
+    control?: Control<TFormValues>
+    disabled?: boolean
+    name: UseControllerProps<TFormValues>['name']
 }
 
-const Checkbox = (props: ICheckboxProps) => {
-    const [checked, setChecked] = useState<boolean>(props.checked || false)
+const Checkbox = <TFormValues extends FieldValues = FieldValues, TValue = string>(
+    props: ICheckboxProps,
+) => {
+    const formContext = useFormContext<TFormValues>()
 
-    const handleCheckboxChange = (event: TCheckboxChangeEvent) => {
-        setChecked(event.target.checked)
+    const renderCheckbox = ({ field }: { field: any }) => (
+        <label className={twMerge(`mb-[10px] inline-flex gap-2 select-none`, props.className)}>
+            <input
+                type="radio"
+                value={props.value as unknown as string}
+                checked={field.value === props.value}
+                onChange={() => field.onChange(props.value)}
+                disabled={props.disabled}
+                className="accent-black-1 border-black-1 h-4 w-4 -translate-y-[-2px] border outline-none hover:shadow-none focus:ring-0 focus:outline-none"
+            />
+            <span className="text-14-20-regular">{props.label}</span>
+        </label>
+    )
 
-        if (props.onChange) {
-            props.onChange(event)
-        }
+    if (props.control || formContext) {
+        const actualControl = props.control ?? formContext.control
+        return (
+            <Controller
+                name={props.name as UseControllerProps<TFormValues>['name']}
+                control={actualControl as Control<TFormValues, any, TFormValues>}
+                render={renderCheckbox}
+            />
+        )
     }
 
     return (
-        <label
-            className={twMerge(
-                'mb-[10px] inline-flex items-center gap-2 select-none',
-                props.className,
-            )}
-        >
+        <label className={twMerge(`mb-[10px] inline-flex gap-2 select-none`, props.className)}>
             <input
-                type="checkbox"
-                checked={checked}
-                onChange={handleCheckboxChange}
-                className="accent-black-1 border-black-1 h-4 w-4 border outline-none hover:shadow-none focus:ring-0 focus:outline-none"
+                type="radio"
+                name={props.name}
+                value={props.value as unknown as string}
+                disabled={props.disabled}
+                className="accent-black-1 border-black-1 h-4 w-4 -translate-y-[-2px] border outline-none hover:shadow-none focus:ring-0 focus:outline-none"
             />
-            <span className="text-sm">{props.label}</span>
+            <span className="text-14-20-regular">{props.label}</span>
         </label>
     )
 }
--- src/pages/act-create/FieldRenderer.tsx
+++ src/pages/act-create/FieldRenderer.tsx
@@ -4,27 +4,31 @@ import { FC, useState } from 'react'
 import { Button } from '../../components/ui/button'
 import { useMultiCamera } from '../../lib/hooks/useMultiCamera'
 import { Label } from '../../components/ui/label'
+import Radio from '../../components/ui/radio'
+import Checkbox from '../../components/ui/checkbox'
 
 type WizardField = {
-    type: 'input' | 'fileInput' | 'checkbox' | 'radio';
-    title?: string;
-    placeholder?: string;
-    name?: string;
+    type: 'input' | 'fileInput' | 'checkbox' | 'radio'
+    title?: string
+    placeholder?: string
+    name?: string
+    label?: string
+    value?: any
     options?: {
-        title: string;
-        value: string;
-    }[];
-    cameraKey?: string;
-};
+        title: string
+        value: string
+    }[]
+    cameraKey?: string
+}
 
 interface FieldRendererProps {
-    field: WizardField;
-    photos?: Record<string, any>;
-    takePicture?: (type: string) => {};
+    field: WizardField
+    photos?: Record<string, any>
+    takePicture?: (type: string) => {}
 }
 
 export const FieldRenderer: FC<FieldRendererProps> = ({ field, photos, takePicture }) => {
-    const { register, watch, setValue } = useFormContext();
+    const { register, watch, setValue } = useFormContext()
     const [submitError, setSubmitError] = useState<string | null>(null)
 
     const handleTakePicture = async (type: 'counter' | 'seal') => {
@@ -41,45 +45,15 @@ export const FieldRenderer: FC<FieldRendererProps> = ({ field, photos, takePictu
             return (
                 <div className="mb-4">
                     {field.title ? <h1>{field.title}</h1> : null}
-                    <Input
-                        name={field.name}
-                        placeholder={field.placeholder}
-                    />
+                    <Input name={field.name} placeholder={field.placeholder} />
                 </div>
-            );
-        // case 'fileInput': {
-        //     const noAccess = watch('noAccess'); // смотрим на чекбокс
-        //
-        //     const handleTakePicture = async () => {
-        //         if (!takePicture || !field.cameraKey) return;
-        //         try {
-        //             await takePicture(field.cameraKey);
-        //         } catch (error) {
-        //             console.error('Ошибка при создании фото:', error);
-        //         }
-        //     };
-        //
-        //     return (
-        //         <div className="mb-4">
-        //             {field.title && <h1>{field.title}</h1>}
-        //
-        //             <Button
-        //                 type="button"
-        //                 onClick={handleTakePicture}
-        //                 disabled={noAccess}
-        //             >
-        //                 {field.placeholder || 'Добавить фото'}
-        //             </Button>
-        //
-        //             {!noAccess && field.cameraKey && photos?.[field.cameraKey] && (
-        //                 <Label icon="image" text={photos[field.cameraKey].fileName} />
-        //             )}
-        //         </div>
-        //     );
-        // }
-
+            )
+        case 'radio':
+            return <Radio name={field.name} value={field.value} label={field.label} />
+        case 'checkbox':
+            return <Checkbox name={field.name} value={field.value} label={field.label} />
 
         default:
-            return null;
+            return null
     }
-};
+}
